bacca9678a3f93ca871311c22c4008563f42d08b
TheThreshCarry <mehdichioukh@gmail.com>
2025-02-27 01:31:26 +0100

    Update Storybook branding and regenerate documentation assets

--- .storybook/themes-storybook-ui.ts
+++ .storybook/themes-storybook-ui.ts
@@ -31,8 +31,8 @@ export const darkUIStorybook = {
 export const lightUIStorybook = {}
 
 export const commonTheme = {
-    brandTitle: "Shadcn-ui",
-    brandUrl: "https://ui.shadcn.com/",
-    brandTarget: "_blank",
-    brandImage: "https://ui.shadcn.com/favicon-32x32.png",
-  }
+    brandTitle: 'Studya',
+    brandUrl: 'https://studya.io',
+    brandTarget: '_blank',
+    brandImage: 'https://www.studya.io/logo.png',
+}
